61baed791819f7843c508f78012ccdcb44d08a6c
Maddalena <maddalena.prandini@studio.unibo.it>
2025-03-07 13:45:40 +0100

    adding new abstract class

---
+++ src/main/java/PvZ/model/impl/AbstractEntity.java
@@ -0,0 +1,19 @@
+package PvZ.model.impl;
+
+import PvZ.model.api.Entity;
+import PvZ.utilities.Position;
+
+public abstract class AbstractEntity implements Entity {
+    private Position position;
+
+    @Override
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    @Override
+    public Position getPosition() {
+        return position;
+    }
+
+}
