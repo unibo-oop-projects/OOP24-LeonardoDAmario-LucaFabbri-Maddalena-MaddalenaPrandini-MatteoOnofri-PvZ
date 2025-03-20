c2559b433aac94b79ea26548d0e9ae27d8f71832
Leonardo D'Amario <leonardo.damario@studio.unibo.it>
2025-03-20 12:21:41 +0100

    minor chillings in SunImpl, but not sure tho in that chillings

--- src/main/java/PvZ/model/impl/SunImpl.java
+++ src/main/java/PvZ/model/impl/SunImpl.java
@@ -5,11 +5,13 @@ import PvZ.model.api.Sun;
 public class SunImpl implements Sun{
     
     private final int VALUE;
+    private boolean alive = true;
 
     @Override
     public void incrementSunCounter() { //prende er contatore e l'aumenta de value
-        final SunCounter SunCounter = this.getSunCounter();
+        final SunCounter sunCounter = GameModelImpl.getSunCounter(); //NOT SURE IF SUNCOUNTER IS IN THE GAMEMODEL
         SunCounter.increaseValue(this.VALUE);
+        this.alive = false;
     }
 
     
