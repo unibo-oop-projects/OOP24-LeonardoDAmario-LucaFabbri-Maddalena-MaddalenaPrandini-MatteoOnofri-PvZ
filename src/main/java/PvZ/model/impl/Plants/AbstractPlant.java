f025f2a1ca20e5c5280bfe816f8b6a81aa678ebf
Maddalena Prandini <maddalena.prandini@studio.unibo.it>
2025-04-06 15:16:31 +0200

    adding an abstact class to handle the different action of the plants using a templates methode

---
+++ src/main/java/PvZ/model/impl/Plants/AbstractPlant.java
@@ -0,0 +1,52 @@
+package PvZ.model.impl.Plants;
+
+import PvZ.model.api.EntitiesManager;
+import PvZ.model.api.Plant;
+import PvZ.model.api.PlantType;
+import PvZ.utilities.Position;
+
+public abstract class AbstractPlant implements Plant{
+    protected int life;
+    protected PlantType type;
+    protected Position position;
+
+    public AbstractPlant(PlantType type) {
+        this.type = type;
+        this.life = type.getLife();
+    }
+
+    public final void plantAction(){
+        if(isAlive()){
+            this.action();
+        }
+        else {
+            System.out.println("The plant is dead and cannot perform any action.");
+        }
+    }
+
+    protected abstract void action();
+
+    @Override
+    public void update(long deltaTime, EntitiesManager entitiesManager) {
+        this.plantAction();
+    }
+
+    @Override
+    public int getLife() {
+        return this.life;
+    }
+    @Override
+    public void decreaseLife(int damage) {
+        this.life -= damage;
+    }
+    @Override
+    public PlantType getType() {
+        return this.type;
+    }
+
+    @Override
+    public Position getPosition() {
+        return this.position;
+    }
+    
+}
