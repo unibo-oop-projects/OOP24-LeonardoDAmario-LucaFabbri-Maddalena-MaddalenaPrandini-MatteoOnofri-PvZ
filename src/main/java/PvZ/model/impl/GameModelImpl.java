6bcc59b2548b007e4df3aa9cb2d8163a30cd2770
Maddalena Prandini <maddalena.prandini@studio.unibo.it>
2025-04-06 14:41:14 +0200

    changing the update methode with the new DeltaTime

--- src/main/java/PvZ/model/impl/GameModelImpl.java
+++ src/main/java/PvZ/model/impl/GameModelImpl.java
@@ -1,24 +1,18 @@
 package PvZ.model.impl;
-
-import java.util.List;
-import java.util.stream.Stream;
-
+import PvZ.model.api.EntitiesManager;
 import PvZ.model.api.Entity;
 import PvZ.model.api.GameModel;
-import PvZ.model.api.Zombie;
 public class GameModelImpl implements GameModel {
-    private List<Entity> entities;
-    private SunCounter sunCounter;
-    
+    EntitiesManager entitiesManager;
+
+    public GameModelImpl(EntitiesManager entitiesManager) {
+        this.entitiesManager = entitiesManager;
+    }
+
     @Override
     public void startGame() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'startGame'");
-    }
-
-    @Override
-    public void updateGame() {
-        this.entities.forEach(Entity::update);
     }
 
     @Override
@@ -35,17 +29,9 @@ public class GameModelImpl implements GameModel {
 
     @Override
     public void update(long deltaTime) {
-        
+        this.entitiesManager.getEntities().forEach(e->e.update(deltaTime, entitiesManager));
     }
-
-    public List<Entity> getEntityList() {
-        return this.entities;
-    }
-
-    public SunCounter getSunCounter() {
-        return this.sunCounter;
-    }
-
+    
     
 
 }
