f75e2abda6db65fa1da5eb7647eef1be3a88eb15
Maddalena Prandini <maddalena.prandini@studio.unibo.it>
2025-04-11 15:15:24 +0200

    implementing delta time for updates

--- src/main/java/PvZ/model/impl/Plants/PeaShooter.java
+++ src/main/java/PvZ/model/impl/Plants/PeaShooter.java
@@ -2,18 +2,23 @@ package PvZ.model.impl.Plants;
 
 
 import PvZ.model.api.EntitiesManager;
-import PvZ.model.api.Plant;
 import PvZ.model.api.PlantType;
-import PvZ.model.impl.AbstractEntity;
 import PvZ.model.impl.BulletImpl;
+import PvZ.utilities.Position;
 
 public class PeaShooter extends AbstractPlant{
+    private double elapsedTime=0;
+    private final double fireRate = 2.0;
     public PeaShooter() {
         super(PlantType.PEASHOOTER);
     }
 
     @Override
-    protected void action(EntitiesManager entitiesManager) {
-        entitiesManager.addEntity(new BulletImpl(this.getPosition()));
+    protected void action(long deltaTime, EntitiesManager entitiesManager) {
+        elapsedTime+=deltaTime;
+        if(elapsedTime>=fireRate){
+            entitiesManager.addEntity(new BulletImpl(this.getPosition()));
+            elapsedTime=0;
+        }
     }
 }
