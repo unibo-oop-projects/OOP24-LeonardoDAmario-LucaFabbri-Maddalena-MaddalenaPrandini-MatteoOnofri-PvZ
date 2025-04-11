a678073d69d8b0ddad50e5cdf5f0318b7fe7a73a
Maddalena Prandini <maddalena.prandini@studio.unibo.it>
2025-04-11 15:16:43 +0200

    implementing delta time for sunflower

--- src/main/java/PvZ/model/impl/Plants/Sunflower.java
+++ src/main/java/PvZ/model/impl/Plants/Sunflower.java
@@ -2,17 +2,22 @@ package PvZ.model.impl.Plants;
 
 import PvZ.model.api.EntitiesManager;
 import PvZ.model.api.PlantType;
+import PvZ.model.impl.BulletImpl;
 
 public class Sunflower extends AbstractPlant{
-    EntitiesManager entitiesManager;
     private final int SUN_VALUE = 25;
+    private double elapsedTime=0;
+    private final double fireRate = 5.0;
     public Sunflower() {
         super(PlantType.SUNFLOWER);
     }
 
     @Override
-    protected void action() {
-        this.entitiesManager.addSun(this.SUN_VALUE);
+    protected void action(long deltaTime, EntitiesManager entitiesManager) {
+        elapsedTime+=deltaTime;
+        if(elapsedTime>=fireRate) {
+            entitiesManager.addSun(this.SUN_VALUE);
+            elapsedTime = 0;
+        }
     }
-
 }
