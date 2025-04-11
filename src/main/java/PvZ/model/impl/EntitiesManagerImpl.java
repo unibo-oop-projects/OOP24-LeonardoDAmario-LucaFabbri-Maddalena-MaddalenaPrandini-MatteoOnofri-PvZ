097d3911fbb3657f2d9f41397c663a64beb7befc
Maddalena Prandini <maddalena.prandini@studio.unibo.it>
2025-04-11 14:12:49 +0200

    implementing new methode

--- src/main/java/PvZ/model/impl/EntitiesManagerImpl.java
+++ src/main/java/PvZ/model/impl/EntitiesManagerImpl.java
@@ -8,6 +8,15 @@ import PvZ.model.api.Entity;
 
 public class EntitiesManagerImpl implements EntitiesManager{
     Set<Entity> entities = new HashSet<>();
+    private static final int DEFAULTSUNS = 50;
+    private static final int DEFAULTKILLS = 0;
+    private int sunCount;
+    private int killCount;
+
+    public EntitiesManagerImpl() {
+        this.sunCount = DEFAULTSUNS;
+        this.killCount = DEFAULTKILLS;
+    }
 
     @Override
     public void addEntity(Entity entity) {
@@ -22,6 +31,35 @@ public class EntitiesManagerImpl implements EntitiesManager{
     @Override
     public Set<Entity> getEntities() {
         return Set.copyOf(this.entities);
+    }
+
+    @Override
+    public void addSun(int amount) {
+        this.sunCount += amount;
+    }
+
+    @Override
+    public boolean spendSun(int amount) {
+        if (sunCount >= amount) {
+            sunCount -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void addKill() {
+        this.killCount += 1;
+    }
+
+    @Override
+    public int getKillCount() {
+        return killCount;
+    }
+
+    @Override
+    public int getSunCount() {
+        return sunCount;
     }
 
 }
