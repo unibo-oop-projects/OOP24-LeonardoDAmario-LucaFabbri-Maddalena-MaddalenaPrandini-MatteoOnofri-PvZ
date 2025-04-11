9513f6f9da519ead571f43f9ad138a0c9d6ed07a
Maddalena Prandini <maddalena.prandini@studio.unibo.it>
2025-04-11 14:13:55 +0200

    implementing action

--- src/main/java/PvZ/model/impl/Plants/PeaShooter.java
+++ src/main/java/PvZ/model/impl/Plants/PeaShooter.java
@@ -1,18 +1,19 @@
 package PvZ.model.impl.Plants;
 
 
+import PvZ.model.api.EntitiesManager;
 import PvZ.model.api.PlantType;
 import PvZ.model.impl.BulletImpl;
 
 public class PeaShooter extends AbstractPlant{
-    
+    EntitiesManager entitiesManager;
     public PeaShooter() {
         super(PlantType.PEASHOOTER);
     }
 
     @Override
     protected void action() {
-        new BulletImpl(this.position);
+        this.entitiesManager.addEntity(new BulletImpl(this.getPosition()));
     }
     
 }
