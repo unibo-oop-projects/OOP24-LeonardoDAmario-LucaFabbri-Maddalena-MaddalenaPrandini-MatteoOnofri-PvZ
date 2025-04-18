d0c680c1e7ff7efb9783b31cdee42db02f70a70c
Maddalena Prandini <maddalena.prandini@studio.unibo.it>
2025-04-18 15:15:40 +0200

    minor changes

--- src/main/java/PvZ/model/impl/Plants/PlantFactory.java
+++ src/main/java/PvZ/model/impl/Plants/PlantFactory.java
@@ -1,7 +1,7 @@
 package PvZ.model.impl.Plants;
-import PvZ.model.api.EntitiesManager;
-import PvZ.model.api.Plant;
-import PvZ.model.api.PlantType;
+import PvZ.model.api.Entities.EntitiesManager;
+import PvZ.model.api.Plants.Plant;
+import PvZ.model.api.Plants.PlantType;
 import PvZ.model.impl.BulletImpl;
 import PvZ.utilities.Position;
 import java.util.Objects;
--- src/main/java/PvZ/model/impl/SunCounter.java
+++
@@ -1,5 +0,0 @@
-package PvZ.model.impl;
-
-public class SunCounter {
-
-}
