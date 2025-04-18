cfa65bcc52711fbfcfedaec952110bdd23892633
Maddalena Prandini <maddalena.prandini@studio.unibo.it>
2025-04-18 17:16:00 +0200

    adding hitbox to constsructor

--- src/main/java/PvZ/model/impl/Plants/AbstractPlant.java
+++ src/main/java/PvZ/model/impl/Plants/AbstractPlant.java
@@ -2,6 +2,7 @@ package PvZ.model.impl.Plants;
 
 import PvZ.model.api.Entities.EntitiesManager;
 import PvZ.model.api.Plants.Plant;
+import PvZ.model.impl.Collisions.HitBoxFactory.HitBoxType;
 import PvZ.model.impl.Entitities.AbstractEntity;
 import PvZ.utilities.Position;
 
@@ -9,7 +10,7 @@ public abstract class AbstractPlant extends AbstractEntity implements Plant{
     private int damage;
 
     public AbstractPlant(Position position) {
-        super(position);
+        super(position, HitBoxType.PLANT);
     }
 
     @Override
