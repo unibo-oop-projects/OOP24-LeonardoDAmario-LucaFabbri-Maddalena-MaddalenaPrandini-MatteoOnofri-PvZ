f6ba8cc1bf9cde1b63c434cb588726f69b873599
Maddalena Prandini <maddalena.prandini@studio.unibo.it>
2025-04-25 16:56:21 +0200

    implement method for mapping the Entity whith GameEntity in this way you know the EntityType that are prsent in the game

--- src/main/java/PvZ/model/api/GameModel.java
+++ src/main/java/PvZ/model/api/GameModel.java
@@ -1,7 +1,11 @@
 package PvZ.model.api;
 
 import PvZ.model.api.Plants.PlantType;
+import PvZ.utilities.EntityType;
+import PvZ.utilities.GameEntity;
 import PvZ.utilities.Position;
+
+import java.util.Set;
 
 public interface GameModel {
     void startGame();
@@ -10,4 +14,5 @@ public interface GameModel {
     GameStatus getGameStatus();
     void placePlant(PlantType type, Position position);
     void update(long deltaTime);
+    Set<GameEntity> getGameEntities();
 }
