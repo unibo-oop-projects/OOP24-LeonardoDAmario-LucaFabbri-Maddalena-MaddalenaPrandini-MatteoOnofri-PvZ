ce72f57ab15bcf60920e33092377c02132553871
Maddalena Prandini <maddalena.prandini@studio.unibo.it>
2025-04-25 16:57:43 +0200

    create plantRoaster panel

---
+++ src/main/java/PvZ/view/impl/Plants/PlantRoaster.java
@@ -0,0 +1,29 @@
+package PvZ.view.impl.Plants;
+
+import PvZ.controller.api.ViewListener;
+import PvZ.view.api.GameView;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PlantRoaster extends JPanel {
+
+    private static final int ROWS = 1;
+    private static final int COLUMNS = 3;
+
+    public PlantRoaster(ViewListener listener) {
+        setLayout(new GridLayout(ROWS, COLUMNS));
+
+        JButton peashooter = new JButton("Peashooter");
+        JButton sunflower = new JButton("Sunflower");
+        JButton wallnut = new JButton("Wallnut");
+
+        peashooter.addActionListener(e -> listener.processInputRoaster(ViewListener.UserInputRoaster.PEASHOOTER));
+        sunflower.addActionListener(e -> listener.processInputRoaster(ViewListener.UserInputRoaster.SUNFLOWER));
+        wallnut.addActionListener(e -> listener.processInputRoaster(ViewListener.UserInputRoaster.WALLNUT));
+
+        add(peashooter);
+        add(sunflower);
+        add(wallnut);
+    }
+}
