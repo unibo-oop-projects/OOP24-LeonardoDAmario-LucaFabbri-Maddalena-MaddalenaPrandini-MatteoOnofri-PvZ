b874d95efca8242559593115df436bb3f00acf25
Luca Fabbri <luca.fabbri53@studio.unibo.it>
2025-04-28 17:47:35 +0200

    Changing menù colors

--- src/main/java/PvZ/view/impl/Menu/MenuView.java
+++ src/main/java/PvZ/view/impl/Menu/MenuView.java
@@ -15,16 +15,15 @@ public class MenuView extends JPanel {
     public MenuView() {
         this.setLayout(new BorderLayout());
 
+
         JLabel titleLabel = new JLabel("Piante contro Zombie", SwingConstants.CENTER);
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 32));
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 40));
         this.add(titleLabel, BorderLayout.NORTH);
 
-        // Etichetta difficoltà
         difficultyLabel = new JLabel("Difficoltà: Normale", SwingConstants.CENTER);
-        difficultyLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+        difficultyLabel.setFont(new Font("Arial", Font.BOLD, 28));
         this.add(difficultyLabel, BorderLayout.SOUTH);
 
-        // Pulsanti
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new GridLayout(3, 1, 10, 10));
         playButton = new JButton("Gioca");
@@ -37,6 +36,21 @@ public class MenuView extends JPanel {
 
         this.add(buttonPanel, BorderLayout.CENTER);
         this.setBorder(BorderFactory.createEmptyBorder(50, 150, 50, 150));
+
+        //prova colori
+        this.setBackground(new Color(34, 139, 34));
+        buttonPanel.setBackground(new Color(34, 139, 34));
+
+        //prova stili
+
+        titleLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 40, 0));
+        difficultyLabel.setBorder(BorderFactory.createEmptyBorder(35, 0, 0, 0));
+
+
+        playButton.setFont(new Font("Arial", Font.PLAIN, 20));
+        difficultyButton.setFont(new Font("Arial", Font.PLAIN, 20));
+        exitButton.setFont(new Font("Arial", Font.PLAIN, 20));
+
     }
 
     public void updateDifficultyLabel(Difficulty difficulty) {
