1e529167aa5f7d68929c5fa4612580c0d3447376
Maddalena Prandini <maddalena.prandini@studio.unibo.it>
2025-04-29 21:51:55 +0200

    change CellButton to colour just a part of the jbutton

--- src/main/java/PvZ/view/impl/Game/grid/CellButton.java
+++ src/main/java/PvZ/view/impl/Game/grid/CellButton.java
@@ -1,14 +1,32 @@
 package PvZ.view.impl.Game.grid;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class CellButton extends JButton {
+    private Color innerColor = null;
+
     public CellButton(int row, int col) {
         super();
-        this.setOpaque(false);
-        this.setContentAreaFilled(false);
-        this.setBorderPainted(false);
-        this.setFocusPainted(false);
         this.setActionCommand(row + "," + col);
+        this.setFocusPainted(false);
+        this.setBorderPainted(true);
+        this.setContentAreaFilled(true);
+        this.setOpaque(true);
+    }
+
+    public void setInnerColor(Color color) {
+        this.innerColor = color;
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (innerColor != null) {
+            g.setColor(innerColor);
+            int margin = 20;
+            g.fillRect(margin, margin, getWidth() - 2 * margin, getHeight() - 2 * margin);
+        }
     }
 }
