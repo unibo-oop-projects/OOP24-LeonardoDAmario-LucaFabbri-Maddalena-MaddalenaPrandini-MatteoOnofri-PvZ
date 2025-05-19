b598381346e8d984038955e8c8ffaea5163510d1
Maddalena Prandini <maddalena.prandini@studio.unibo.it>
2025-05-19 22:14:48 +0200

    update checkstyle

--- src/main/java/pvz/model/api/entities/Entity.java
+++ src/main/java/pvz/model/api/entities/Entity.java
@@ -1,14 +1,42 @@
 package pvz.model.api.entities;
 
 import pvz.model.api.Collisions.HitBox;
-import pvz.utilities.*;
+import pvz.utilities.Position;
 
+/**
+ * Interface representing a generic entity in the game.
+ * <p>
+ * All game entities (e.g., plants, zombies, bullets) must implement this interface.
+ * Provides methods for managing position, collision, and periodic updates.
+ */
 public interface Entity {
-    //setting delle posizioni uguali per tutte le entità
-    void setPosition(Position Position);
+
+    /**
+     * Sets the position of the entity.
+     *
+     * @param position the new position; must not be {@code null}.
+     */
+    void setPosition(Position position);
+
+    /**
+     * Retrieves the current position of the entity.
+     *
+     * @return the current {@link Position}.
+     */
     Position getPosition();
 
-    //updating plants
+    /**
+     * Updates the state of the entity based on the elapsed time.
+     *
+     * @param deltaTime the time since the last update in milliseconds.
+     * @param entitiesManager the manager responsible for all entities in the game.
+     */
     void update(long deltaTime, EntitiesManager entitiesManager);
+
+    /**
+     * Gets the hitbox used for collision detection.
+     *
+     * @return the {@link HitBox} of this entity.
+     */
     HitBox getHitBox();
 }
