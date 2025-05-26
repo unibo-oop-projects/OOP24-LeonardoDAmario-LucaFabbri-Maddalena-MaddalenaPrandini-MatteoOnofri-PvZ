751710316f1c6c332ed53fe0e30cf343041daef7
Leonardo D'Amario <leonardo.damario@studio.unibo.it>
2025-05-26 22:28:39 +0200

    checkstyles on my class finished

--- src/main/java/pvz/model/api/Bullets/Bullet.java
+++ src/main/java/pvz/model/api/Bullets/Bullet.java
@@ -2,8 +2,17 @@ package pvz.model.api.Bullets;
 
 import pvz.model.api.entities.Entity;
 
-public interface Bullet extends Entity{
+/**
+ * Represents a bullet entity in the game.
+ * A bullet can deal damage to other entities (e.g., zombies).
+ */
+public interface Bullet extends Entity {
 
+    /**
+     * Gets the damage dealt by this bullet.
+     *
+     * @return the damage value
+     */
     int getDamage();
 
 }
