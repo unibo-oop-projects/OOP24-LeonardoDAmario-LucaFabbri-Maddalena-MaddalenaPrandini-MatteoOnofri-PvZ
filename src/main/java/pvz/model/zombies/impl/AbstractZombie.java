50924e8d8a6c0b5301ce8411b53151d8cbeb9bf9
Matteo Onofri <matteo.onofri4@studio.unibo.it>
2025-06-16 17:12:22 +0200

    resolving merge issues

--- src/main/java/pvz/model/zombies/impl/AbstractZombie.java
+++ src/main/java/pvz/model/zombies/impl/AbstractZombie.java
@@ -13,15 +13,32 @@ import pvz.utilities.Position;
 
 import java.util.Optional;
 
+/**
+ * Abstract base class for all zombie types in the game.
+ * Implements common behavior such as health tracking, movement, and collision handling.
+ */
 abstract class AbstractZombie extends AbstractEntity implements Zombie {
 
+    /** The current health points of the zombie. */
     protected int health;
+    /** The movement speed of the zombie. */
     protected int speed;
+    /** Whether the zombie is alive. */
     protected boolean alive;
+    /** The collision manager for handling collisions. */
     protected CollisionManager collisionManager;
+    /** The rate at which the zombie can attack (in milliseconds). */
     protected static final long ATTACK_RATE = 2000;
+    /** The time since the last attack. */
     protected long lastAttackTime = 0;
 
+    /**
+     * Constructs an AbstractZombie with the specified position, health, and speed.
+     *
+     * @param position the initial position of the zombie.
+     * @param health   the initial health of the zombie.
+     * @param speed    the movement speed of the zombie.
+     */
     public AbstractZombie(Position position, int health, int speed) {
         super(position, HitBoxType.ZOMBIE);
         this.health = health;
@@ -31,6 +48,9 @@ abstract class AbstractZombie extends AbstractEntity implements Zombie {
         this.lastAttackTime = ATTACK_RATE;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void update(long deltaTime, EntitiesManager entitiesManager) {
 
@@ -51,7 +71,11 @@ abstract class AbstractZombie extends AbstractEntity implements Zombie {
         }
     }
 
-    
+    /**
+     * Moves the zombie based on its speed and the elapsed time.
+     *
+     * @param deltaTime the time elapsed since the last update (in ms).
+     */
     private void move(long deltaTime) {
         final double move = this.speed * (1 / 100.0); 
         final double newX = this.getPosition().x() - move; 
@@ -59,38 +83,58 @@ abstract class AbstractZombie extends AbstractEntity implements Zombie {
         this.getHitBox().update(this.getPosition()); 
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void takeDamage(int damage) {
         health -= damage;
         if (health < 0) alive = false;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean isAlive() {
         return alive && health > 0;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void forceKill() {
         alive = false;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getHealth() {
         return health;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getSpeed() {
         return speed;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public HitBox getHitBox() {
         return super.getHitBox();
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void decreaseLife(int damage) {
         this.health -= damage;
@@ -99,10 +143,15 @@ abstract class AbstractZombie extends AbstractEntity implements Zombie {
         }
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public abstract int getDamage();
-    
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public abstract ZombieType getType();
 }
